c9f5f146292a46843ed6f4b05b8372e4b314f517
vishant101 <mini-j.singh@live.com>
2019-12-10 08:59:40 +1300

    Basic webview communication

--- app/views/ReaderArticle.js
+++ app/views/ReaderArticle.js
@@ -14,37 +14,46 @@ import css from '../styles/CSS'
 class ReaderArticle extends Component {
 	constructor(props) {
 		super(props)
+		this.webView = null
 	}
 
-	
 	onNextPress(){
-		
+		this.webView.postMessage('Hello from RN')
 	}
 
 	onPrevPress(){
-		
+		console.log(this.webView)
+		this.webView.postMessage('Hello from RN')
 	}
 
+	_reWebView = (webView) => {
+		this.webView = webView
+	}
 	
 	render() {
 		const { articles, articleIndex } = this.props
 		const article = articles[articleIndex]
 
+		const jsCode = `
+    		document.addEventListener("message", function(data) {
+				alert(data.data);
+			})`
+
 		return (
 			<View style={{ flex: 1 }}>
 				<WebView 
-					ref={webview => { this.myWebview = webview }}
+					ref={this._reWebView}
 					source= {{ uri: article.url }}
-					style={{flex: 1  }}
+					style={{ flex: 1 }}
+					injectedJavaScript={jsCode}
+          			javaScriptEnabled={true}
 				/>
 				<View style={{ flexDirection: 'row', justifyContent: 'space-between'}}>
 					<Button text={BUTTONTEXT.PREV} onPress={() => this.onPrevPress()} />
 					<Button text={BUTTONTEXT.NEXT} onPress={() => this.onNextPress()} />
 				</View>
 			</View>
-			
 		)
-			
 	}
 }
 
